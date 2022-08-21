17b156bde096aadefe8280c7876264480edf039c
Arpan Dhama <arpandhama2001@gmail.com>
2022-08-21 15:59:14 +0530

    logo and icon changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { ReactComponent as Icon } from "./clouds-svgrepo-com.svg";
 
-// require("dotenv").config();
-
-// const location = 101264773;
-
 // const lat = 28.723151;
 // const lon = 77.302066;
 
@@ -13,16 +9,13 @@ const App = () => {
   const [temp, setTemp] = useState(0);
   const [wind, setWind] = useState(0);
   const [gust, setGust] = useState(0);
+  const URL = "https://foreca-weather.p.rapidapi.com/current/101264773";
 
   const options = {
-    method: "GET",
-    url: "https://foreca-weather.p.rapidapi.com/current/101264773",
     params: {
       alt: "0",
       tempunit: "C",
-      windunit: "MS",
-      tz: "Europe/London",
-      lang: "en",
+      windunit: "KMH",
     },
     headers: {
       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_KEY,
@@ -30,20 +23,21 @@ const App = () => {
     },
   };
 
+  const fetchData = async () => {
+    try {
+      const {
+        data: { current },
+      } = await axios.get(URL, options);
+      setTemp(current.temperature);
+      setWind(current.windSpeed);
+      setGust(current.windGust);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    return () => {
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data);
-          setTemp(response.data.current.temperature);
-          setWind(response.data.current.windSpeed * (18 / 5));
-          setGust(response.data.current.windGust * (18 / 5));
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    };
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
